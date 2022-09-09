b3ff3cca2d1c148e0fc77e72a0882ac2029a9fce
GujjetiPranitha <111724196+GujjetiPranitha@users.noreply.github.com>
2022-09-09 11:39:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+var cors=require('cors')
+const port=process.env.PORT || 3001
+//we use app for if there is any environment variable port or the other port
+app.use(express.json())
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen(port,function(req,res){
+    console.log("Server started")
+})
